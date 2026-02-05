6b5d2db1b88bae326bc2f4f84cb43278fd90e13f
krystal <krystal93002232@gmail.com>
2026-02-05 12:28:59 +0800

    chore: add certifi to fix certificate verify failed

--- config/initializers/line_bot.rb
+++ config/initializers/line_bot.rb
@@ -1,4 +1,7 @@
 require 'line/bot'
+require 'certifi'
+
+ENV['SSL_CERT_FILE'] = Certifi.where
 
 LINE_CLIENT = Line::Bot::Client.new do |config|
   config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
